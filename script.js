e181f06cadee94997f3d6e0a3fdab412fc56dd2b
Aurelien Gremy <agremy@solutionsmedias360.com>
2021-03-31 23:29:59 -0400

    refactor(all files) script, html, css - script : all functions - html : base - css : empty

---
+++ script.js
@@ -0,0 +1,87 @@
+import { quoteList } from "./quotesList.js";
+
+const btn = document.querySelector("#btn");
+const quote = document.querySelector("#quote");
+
+const nbrSeason = 7;
+
+const epBySeasonOneToFour = 22;
+const epBySeasonFive = 20;
+const epBySeasonSix = 13;
+const epBySeasonSeven = 8;
+
+function randomNbr(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function addZero(nbr) {
+  if (nbr.toString().length == 1) {
+    return "0" + nbr;
+  }
+  return nbr;
+}
+
+let seasonNoChosen = randomNbr(1, nbrSeason);
+
+function chooseSeason(nbr) {
+  if (nbr == 5) {
+    return epBySeasonFive;
+  }
+  if (nbr == 6) {
+    return epBySeasonSix;
+  }
+  if (nbr == 7) {
+    return epBySeasonSeven;
+  } else {
+    return epBySeasonOneToFour;
+  }
+}
+
+console.log("saison : " + seasonNoChosen);
+// saison choisi
+
+let nbrEpChosenSeason = chooseSeason(seasonNoChosen);
+console.log("Nbr Ep : " + nbrEpChosenSeason);
+// nbre d'episode par saison
+
+function chooseEp(nbrSeason) {
+  return randomNbr(1, nbrSeason);
+}
+
+let chosenEpFromSeason = chooseEp(nbrEpChosenSeason);
+console.log("No Ep de la saison choisi : " + chosenEpFromSeason);
+// No de l'episode de la saison choisi
+
+let chosenEpFromSeasonWithZero = addZero(chosenEpFromSeason);
+console.log(
+  "No Ep de la saison choisi (avec zero) : " + chosenEpFromSeasonWithZero
+);
+// No de l'episode de la saison choisi avec Zero
+
+function chooseEpAndSeason(season, episode) {
+  return "S" + season + "E" + episode;
+}
+
+let SxxEpxx = chooseEpAndSeason(seasonNoChosen, chosenEpFromSeasonWithZero);
+
+console.log(SxxEpxx);
+console.log(SxxEpxx.toString());
+
+SxxEpxx.toString();
+
+let result = quoteList[SxxEpxx];
+
+quote.innerHTML = result;
+
+function initQuoteList() {
+  // let seasonNoChosen = randomNbr(1, nbrSeason);
+  // let nbrEpChosenSeason = chooseSeason(seasonNoChosen);
+  // let chosenEpFromSeason = chooseEp(nbrEpChosenSeason);
+  // let chosenEpFromSeasonWithZero = addZero(chosenEpFromSeason);
+  // let SxxEpxx = chooseEpAndSeason(seasonNoChosen, chosenEpFromSeasonWithZero);
+  // SxxEpxx.toString();
+  // let result = quoteList[SxxEpxx];
+  // quote.innerHTML = result;
+}
+
+btn.addEventListener("click", initQuoteList);
